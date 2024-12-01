ebdf4808c58e3541fe66dd3113784030df5f1ef3
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-02 01:09:39 +0530

    database connection

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv";
+import connectDB from './utils/db.js';
 
 
 dotenv.config();
@@ -11,6 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
+// app.use("api/h3/user", userRoute);
+
+
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server is runing at port ${PORT}`);
 })
