246bc100997499756c47e4de66c62b27c34d2add
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-02 00:41:44 +0530

    first commit

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is runing at port ${PORT}`);
+})
