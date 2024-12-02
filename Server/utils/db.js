11731f1502fcfe21f20473e81e76d6e82a84d4ec
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-02 13:08:41 +0530

    model

--- Server/utils/db.js
+++ Server/utils/db.js
@@ -4,7 +4,7 @@ const connectDB = async ()=> {
 
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Mongo connected");
+        console.log("MongoDB connected succesfully");
     } catch (error) {
         console.log("DB not connected");
     }
